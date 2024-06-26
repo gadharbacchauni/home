ed0593e5c9b3fe405f54db1b9a3e8c9efa2d2ec1
gadharbacchauni <gadharbacchauni@gmail.com>
2024-06-27 00:13:38 +0530

    herobg

---
+++ herobg.js
@@ -0,0 +1,22 @@
+var images = [
+	"shrek.jpg",
+	"donk.png",
+];
+$(function () {
+	var i = 0;
+	$("#hero").css("background-image", "url(herobg/" + images[i] + ")");
+	$("#hero").css("background-position-x", "center");
+	$("#hero").css("background-position-y", "center");
+	$("#hero").css("background-size", "cover");
+	setInterval(function () {
+        console.log(`${i} ${images.length} ${images[i]}`);
+		i++;
+		if (i == images.length) {
+			i = 0;
+		}
+		$("#hero").fadeOut("slow", function () {
+			$(this).css("background-image", "url(herobg/" + images[i] + "");
+			$(this).fadeIn("slow");
+		});
+	}, 2500);
+});
